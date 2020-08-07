eab7f5e91da8d182073287d93e31385012a16ef1
Ravi-Sharma792 <ravi14092001@gmail.com>
2020-08-07 14:15:13 +0530

    done

--- lib/Screens/signup_screen.dart
+++ lib/Screens/signup_screen.dart
@@ -2,9 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:task_app/Widgets/login_bars.dart';
 import 'package:task_app/Widgets/social_media_bar.dart';
+import '../Models/auth_form.dart';
 // import 'package:flare_flutter/flare_actor.dart';
 
-class SignUpScreen extends StatelessWidget {
+class SignUpScreen extends StatefulWidget {
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final formKey = GlobalKey<FormState>();
+
+  void trysubmit() {
+    final isValid = formKey.currentState.validate();
+
+    if (isValid) {
+      formKey.currentState.save();
+      print(username);
+      print(email);
+      print(password);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // double deviceHeight = MediaQuery.of(context).size.height;
@@ -19,72 +38,74 @@ class SignUpScreen extends StatelessWidget {
             children: <Widget>[
               Padding(
                 padding: EdgeInsets.fromLTRB(20, 190, 20, 20),
-                child: Column(
-                  children: <Widget>[
-                    LoginBars(Icons.account_circle, 'Username'),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    LoginBars(Icons.email, 'E-mail'),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    LoginBars(Icons.lock, 'Password'),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    RaisedButton(
-                      color: Theme.of(context).primaryColor,
-                      onPressed: () =>
-                          Navigator.of(context).pushNamed('/login'),
-                      child: Text(
-                        'SignUp',
-                        style: Theme.of(context).textTheme.button,
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    children: <Widget>[
+                      LoginBars(Icons.account_circle, 'Username'),
+                      SizedBox(
+                        height: 10.0,
                       ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25.0),
+                      LoginBars(Icons.email, 'E-mail'),
+                      SizedBox(
+                        height: 10.0,
                       ),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Container(
-                      height: 1,
-                      width: deviceWidth,
-                      color: Colors.grey[200],
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    RaisedButton(
-                      color: Theme.of(context).primaryColor,
-                      onPressed: () =>
-                          Navigator.of(context).pushNamed('/login'),
-                      child: Text(
-                        'Login',
-                        style: Theme.of(context).textTheme.button,
+                      LoginBars(Icons.lock, 'Password'),
+                      SizedBox(
+                        height: 10.0,
                       ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25.0),
+                      RaisedButton(
+                        color: Theme.of(context).primaryColor,
+                        onPressed: trysubmit,
+                        child: Text(
+                          'SignUp',
+                          style: Theme.of(context).textTheme.button,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25.0),
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    SocialMediaBar(
-                      Color(0xff3b5998),
-                      MdiIcons.facebook,
-                      'Sign in with Facebook',
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    SocialMediaBar(
-                      Color(0xffdb4a39),
-                      MdiIcons.googlePlus,
-                      'Sign in with Google',
-                    ),
-                  ],
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Container(
+                        height: 1,
+                        width: deviceWidth,
+                        color: Colors.grey[200],
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      RaisedButton(
+                        color: Theme.of(context).primaryColor,
+                        onPressed: () =>
+                            Navigator.of(context).pushNamed('/login'),
+                        child: Text(
+                          'Login',
+                          style: Theme.of(context).textTheme.button,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25.0),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      SocialMediaBar(
+                        Color(0xff3b5998),
+                        MdiIcons.facebook,
+                        'Sign in with Facebook',
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      SocialMediaBar(
+                        Color(0xffdb4a39),
+                        MdiIcons.googlePlus,
+                        'Sign in with Google',
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
--- lib/Widgets/login_bars.dart
+++ lib/Widgets/login_bars.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../Models/auth_form.dart';
 
 class LoginBars extends StatelessWidget {
   final IconData icon;
@@ -9,12 +10,37 @@ class LoginBars extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      validator: (value) {
+        if (title == 'Username') {
+          if (value.isEmpty || value.length < 4) {
+            return 'Please enter at least 4 characters';
+          }
+        } else if (title == 'E-mail') {
+          if (value.isEmpty || !value.contains('@')) {
+            return 'Please enter a valid email address.';
+          }
+        } else if (title == 'Password') {
+          if (value.isEmpty || value.length < 7) {
+            return 'Password must be at least 7 characters long.';
+          }
+        }
+        return null;
+      },
+      onSaved: (value) {
+        if (title == 'Username') {
+          username = value;
+        } else if (title == 'E-mail') {
+          email = value;
+        } else if (title == 'Password') {
+          password = value;
+        }
+      },
       style: TextStyle(
         fontFamily: 'Raleway',
         color: Colors.black,
       ),
       decoration: InputDecoration(
-        enabledBorder: OutlineInputBorder(
+        border: OutlineInputBorder(
           borderSide: BorderSide(
             color: Theme.of(context).primaryColor,
             width: 2,
